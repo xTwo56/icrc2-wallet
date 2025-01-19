fef8456371e79a3a5f57edbbfb3d4b1c795495ea
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2025-01-19 10:17:57 +0530

    transfer added

--- src/icrc2-wallet-backend/src/lib.rs
+++ src/icrc2-wallet-backend/src/lib.rs
@@ -1,4 +1,38 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use candid::{CandidType, Deserialize, Principal};
+use icrc_ledger_types::icrc1::account::Account;
+use icrc_ledger_types::icrc1::transfer::{BlockIndex, NumTokens, TransferArg, TransferError};
+use serde::Serialize;
+
+#[derive(CandidType, Deserialize, Serialize)]
+pub struct TransferInputArg {
+    amount: NumTokens,
+    to_account: Account,
+}
+
+#[ic_cdk::update]
+async fn transfer(args: TransferInputArg) -> Result<BlockIndex, String> {
+    ic_cdk::println!(
+        "Transferring {} tokens to account {}",
+        &args.amount,
+        &args.to_account,
+    );
+
+    let transfer_args: TransferArg = TransferArg {
+        memo: None,
+        amount: args.amount,
+        from_subaccount: None,
+        fee: None,
+        to: args.to_account,
+        created_at_time: None,
+    };
+
+    ic_cdk::call::<(TransferArg,), (Result<BlockIndex, TransferError>,)>(
+        Principal::from_text("").expect("Could not decode the principal."),
+        "icrc1_transfer",
+        (transfer_args,),
+    )
+    .await
+    .map_err(|e| format!("ledger not found: {:?}", e))?
+    .0
+    .map_err(|e| format!("ledger transfer failed:  {:?}", e))
 }
